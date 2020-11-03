7d959ff990186241ad4dfbefe43129feac91c3fa
pigeon-crystal <64479106+pigeon-crystal@users.noreply.github.com>
2020-11-03 09:01:16 -0600

    ambinni update, hp border bar fix

--- data/events/npc_trades.asm
+++ data/events/npc_trades.asm
@@ -8,8 +8,8 @@ ENDM
 
 NPCTrades:
 ; entries correspond to NPCTRADE_* constants
-	npctrade TRADE_DIALOGSET_COLLECTOR, AMBINNI,    ZASTER,     "HYUNDIE@@@@", $97, $ab, METAL_COAT, 		37460, 		"MIKE@@@@@@@", TRADE_GENDER_EITHER ; goldenrod department
-	npctrade TRADE_DIALOGSET_GIRL,      LAWNIE,     SNOSEBLEED, "SQUATCH@@@@", $57, $99, BITTER_BERRY, 		48926, 		"KYLE@@@@@@@", TRADE_GENDER_FEMALE ; violet city
+	npctrade TRADE_DIALOGSET_COLLECTOR, AMBINNI,    ZASTER,     "HYUNDIE@@@@", $87, $a6, METAL_COAT, 		37460, 		"MIKE@@@@@@@", TRADE_GENDER_EITHER ; goldenrod department
+	npctrade TRADE_DIALOGSET_GIRL,      LAWNIE,     SNOSEBLEED, "SQUATCH@@@@", $57, $69, BITTER_BERRY, 		48926, 		"KYLE@@@@@@@", TRADE_GENDER_FEMALE ; violet city
 	npctrade TRADE_DIALOGSET_HAPPY,     LANATUN,    VOLTORB,   	"VOLTY@@@@@@", $98, $88, PRZCUREBERRY, 		29189, 		"TIM@@@@@@@@", TRADE_GENDER_EITHER ; olivine
 	npctrade TRADE_DIALOGSET_GIRL,      DRAGONAIR,  DODRIO,     "DORIS@@@@@@", $77, $66, SMOKE_BALL,   		00283, 		"EMY@@@@@@@@", TRADE_GENDER_FEMALE ; blackthorn
 	npctrade TRADE_DIALOGSET_NEWBIE,    SCRUFFRUIT, SQUEENIE,   "FONDA@@@@@@", $96, $86, MYSTERYBERRY, 		15616, 		"CHRIS@@@@@@", TRADE_GENDER_EITHER ; pewter
